0c75237fa25b371426f88e2340f2ce056512b3e1
asbjørn <qw41ha@student.aau.dk>
2025-02-26 22:57:57 +0100

    ny hukomlse

--- game_of_life.c
+++ game_of_life.c
@@ -16,6 +16,7 @@ struct bor{
 	int *bor[2];
 };
 
+/*
 int* bor_inedx(struct bor br, int x, int y)
 {
 	return br.bor[br.index_bor] + (br.len_x+br.pading*2)*(y + br.pading) + x + br.pading;
@@ -24,6 +25,91 @@ int* bor_inedx(struct bor br, int x, int y)
 int* bor_inedx_alt(struct bor br, int x, int y)
 {
 	return br.bor[(br.index_bor + 1) % 2] + (br.len_x+br.pading*2)*(y + br.pading) + x + br.pading;
+}
+int *bor_inedx(struct bor br, int x, int y)
+{
+	x += br.pading;
+	y += br.pading;
+	unsigned int i = 0;
+	i += (x & 0b00000001);
+	i += (y & 0b00000001) << 1;
+	i += (x & 0b00000010) << 1;
+	i += (y & 0b00000010) << 2;
+	i += (x & 0b00000100) << 2;
+	i += (y & 0b00000100) << 3;
+	i += (x & 0b00001000) << 3;
+	i += (y & 0b00001000) << 4;
+	i += (x & 0b00010000) << 4;
+	i += (y & 0b00010000) << 5;
+	i += (x & 0b00100000) << 5;
+	i += (y & 0b00100000) << 6;
+	i += (x & 0b01000000) << 6;
+	i += (y & 0b01000000) << 7;
+	i += (x & 0b10000000) << 7;
+	i += (y & 0b10000000) << 8;
+	return br.bor[br.index_bor] + i;
+}
+
+int *bor_inedx_alt(struct bor br, int x, int y)
+{
+	x += br.pading;
+	y += br.pading;
+	unsigned int i = 0;
+	i += (x & 0b00000001);
+	i += (y & 0b00000001) << 1;
+	i += (x & 0b00000010) << 1;
+	i += (y & 0b00000010) << 2;
+	i += (x & 0b00000100) << 2;
+	i += (y & 0b00000100) << 3;
+	i += (x & 0b00001000) << 3;
+	i += (y & 0b00001000) << 4;
+	i += (x & 0b00010000) << 4;
+	i += (y & 0b00010000) << 5;
+	i += (x & 0b00100000) << 5;
+	i += (y & 0b00100000) << 6;
+	i += (x & 0b01000000) << 6;
+	i += (y & 0b01000000) << 7;
+	i += (x & 0b10000000) << 7;
+	i += (y & 0b10000000) << 8;
+	return br.bor[(br.index_bor + 1) % 2] + i;
+}
+
+*/
+
+int* bor_inedx(struct bor br, int x, int y)
+{
+    static const unsigned int MASKS[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+    static const unsigned int SHIFTS[] = {1, 2, 4, 8};
+
+    x = (x | (x << SHIFTS[3])) & MASKS[3];
+    x = (x | (x << SHIFTS[2])) & MASKS[2];
+    x = (x | (x << SHIFTS[1])) & MASKS[1];
+    x = (x | (x << SHIFTS[0])) & MASKS[0];
+
+    y = (y | (y << SHIFTS[3])) & MASKS[3];
+    y = (y | (y << SHIFTS[2])) & MASKS[2];
+    y = (y | (y << SHIFTS[1])) & MASKS[1];
+    y = (y | (y << SHIFTS[0])) & MASKS[0];
+
+	return br.bor[br.index_bor] + (x | (y << 1));
+}
+
+int* bor_inedx_alt(struct bor br, int x, int y)
+{
+    static const unsigned int MASKS[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF};
+    static const unsigned int SHIFTS[] = {1, 2, 4, 8};
+
+    x = (x | (x << SHIFTS[3])) & MASKS[3];
+    x = (x | (x << SHIFTS[2])) & MASKS[2];
+    x = (x | (x << SHIFTS[1])) & MASKS[1];
+    x = (x | (x << SHIFTS[0])) & MASKS[0];
+
+    y = (y | (y << SHIFTS[3])) & MASKS[3];
+    y = (y | (y << SHIFTS[2])) & MASKS[2];
+    y = (y | (y << SHIFTS[1])) & MASKS[1];
+    y = (y | (y << SHIFTS[0])) & MASKS[0];
+
+	return br.bor[(br.index_bor + 1) % 2] + (x | (y << 1));
 }
 
 int gem_til_img(struct bor br ,char *fil_sti, int i)
@@ -149,14 +235,14 @@ void bor_trin(struct bor br)
 int main()
 {
 	struct bor br;
-	if( init(&br, 1920, 1080) )
+	if( init(&br, 1024, 1024))
 	{
-		fprintf(stderr,"faild to inishiale");
+		fprintf(stderr,"faildl to inishiale");
 		return 1;
 	}
 	bor_to_noise(br);
 
-	int n = 10000;
+	int n = 1000;
 	for(int i = 0; i < n; i++)
 	{
 		printf("%d af %d \r", i, n);
