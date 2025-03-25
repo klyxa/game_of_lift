b9a52d824c34ba3b65f8de1c0ee39c8593dd4aee
Asbjørn <kykutura@gmx.com>
2025-03-25 08:33:15 +0100

    pas

--- til_fiml.sh
+++ til_fiml.sh
@@ -1,2 +1,3 @@
 #rm img/img_*.pbm
-ffmpeg -r 30 -i img/img_%d.pbm -vcodec libx264 -crf 25  -pix_fmt yuv420p movi_game_of_life.mp4
+#ffmpeg -r 30 -i img/img_%d.pbm -vcodec libx264 -crf 25  -pix_fmt yuv420p movi_game_of_life.mp4
+ffmpeg -r 30 -i img/img_%d.pbm -vf "crop=iw-mod(iw\,2):ih-mod(ih\,2)" -vcodec libx264  -pix_fmt yuv420p movi_game_of_life.mp4
